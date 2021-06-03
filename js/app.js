8daab8769d3eb07267f8857bba4d9541930f91a5
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2021-06-03 20:50:58 +0530

    Fix the mobile navigation

--- js/app.js
+++ js/app.js
@@ -1,144 +1,84 @@
-const navSlide = () => {
-  const burger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
-  const loginLink = document.querySelector(".login");
-  const signUpBtn = document.querySelector(".sign-up");
-  const introHeader = document.querySelector(".intro-header");
-  const introBtns = document.querySelector(".intro-btns");
+const productAnchor = document.querySelector("#product-anchor");
+const productContent = document.querySelector(".product-content");
+const productArrowIcon = document.querySelector(".arrow-light");
+const companyAnchor = document.querySelector("#company-anchor");
+const companyContent = document.querySelector(".company-content");
+const companyArrowIcon = document.querySelector(".arrow-light-company");
+const connectAnchor = document.querySelector("#connect-anchor");
+const connectContent = document.querySelector(".connect-content");
+const connectArrowIcon = document.querySelector(".arrow-light-connect");
+const arrowDarkProduct = document.querySelector(".arrow-dark-product");
+const arrowDarkCompany = document.querySelector(".arrow-dark-company");
+const arrowDarkConnect = document.querySelector(".arrow-dark-connect");
+const hamburgerMenu = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const mobileMenu = document.querySelector(".mobile-menu");
+const navLinksLI = document.querySelectorAll(".nav-links li");
+const editorImg = document.querySelector(".editor");
 
-  //Toggle NAv
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("close");
-    navLinks.style.display = "block";
+productAnchor.addEventListener("click", () => {
+  productAnchor.classList.toggle("active");
 
-    if (!burger.classList.contains("close")) {
-      navLinks.style.display = "none";
-      signUpBtn.style.display = "none";
-      loginLink.style.display = "none";
-      introHeader.style.display = "block";
-      introBtns.style.display = "block";
-    } else {
-      signUpBtn.style.display = "block";
-      loginLink.style.display = "block";
-      introHeader.style.display = "none";
-      introBtns.style.display = "none";
-    }
+  if (productAnchor.classList.contains("active")) {
+    productContent.style.opacity = "1";
+    productContent.style.pointerEvents = "all";
+    productContent.style.zIndex = "1";
+    productArrowIcon.style.transform = "rotate(180deg)";
+    arrowDarkProduct.style.transform = "rotate(180deg)";
+  } else {
+    productContent.style.opacity = "0";
+    productContent.style.zIndex = "0";
+    productContent.style.pointerEvents = "none";
+    productArrowIcon.style.transform = "rotate(0deg)";
+    arrowDarkProduct.style.transform = "rotate(0deg)";
+  }
+});
+
+companyAnchor.addEventListener("click", () => {
+  companyAnchor.classList.toggle("active");
+
+  if (companyAnchor.classList.contains("active")) {
+    companyContent.style.opacity = "1";
+    companyContent.style.zIndex = "1";
+    companyContent.style.pointerEvents = "all";
+    companyArrowIcon.style.transform = "rotate(180deg)";
+    arrowDarkCompany.style.transform = "rotate(180deg)";
+  } else {
+    companyContent.style.opacity = "0";
+    companyContent.style.zIndex = "0";
+    companyContent.style.pointerEvents = "none";
+    companyArrowIcon.style.transform = "rotate(0deg)";
+    arrowDarkCompany.style.transform = "rotate(0deg)";
+  }
+});
+
+connectAnchor.addEventListener("click", () => {
+  connectAnchor.classList.toggle("active");
+
+  if (connectAnchor.classList.contains("active")) {
+    connectContent.style.opacity = "1";
+    connectContent.style.pointerEvents = "all";
+    connectArrowIcon.style.transform = "rotate(180deg)";
+    arrowDarkConnect.style.transform = "rotate(180deg)";
+  } else {
+    connectContent.style.opacity = "0";
+    connectContent.style.pointerEvents = "none";
+    connectArrowIcon.style.transform = "rotate(0deg)";
+    arrowDarkConnect.style.transform = "rotate(0deg)";
+  }
+});
+
+hamburgerMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  if (navLinks.classList.contains("open")) {
+    navLinks.style.zIndex = "1";
+    mobileMenu.src = "../images/icon-close.svg";
+  } else {
+    navLinks.style.zIndex = "0";
+    mobileMenu.src = "../images/icon-hamburger.svg";
+  }
+
+  navLinksLI.forEach((link) => {
+    link.classList.toggle("fade");
   });
-};
-navSlide();
-
-//Animation
-const slides = document.querySelectorAll(".slide");
-const navHeader = document.querySelector("nav");
-const introHeader = document.querySelector(".intro-header");
-const introBtns = document.querySelector(".intro-btns");
-const header = document.querySelector(".future-heading");
-const futureTextContent = document.querySelector(".future-text-content");
-const futureIllustration = document.querySelector(".future-illustration");
-const mobileIllustration = document.querySelector(".mobile-illustration");
-const infrastructureText = document.querySelector(".infrastructure-text");
-const laptopIllustration = document.querySelector(".illustration-laptop");
-const toolsTextContent = document.querySelector(".tools-text-content");
-const footerContent = document.querySelector(".footer-content");
-const slideTimeline = gsap.timeline({
-  defaults: { duration: 1, ease: "power2.inOut" },
-});
-const elasticTimeline = gsap.timeline({
-  defaults: { duration: 1, ease: "elastic.out" },
-});
-const backTimeline = gsap.timeline({
-  defaults: { duration: 1, ease: "back.out" },
-});
-const bounceTimeline = gsap.timeline({
-  defaults: { duration: 1, ease: "bounce.out" },
-});
-
-gsap.config({ nullTargetWarn: false });
-
-elasticTimeline.fromTo(
-  navHeader,
-  { opacity: 0, x: "50%" },
-  { opacity: 1, x: "0%" }
-);
-slideTimeline.fromTo(
-  introHeader,
-  { opacity: 0, x: "-20%" },
-  { opacity: 1, x: "0%" },
-  "+=1"
-);
-slideTimeline.fromTo(
-  introBtns,
-  { opacity: 0, scale: 0 },
-  { opacity: 1, scale: 1 },
-  "-=1"
-);
-
-let options = {
-  threshold: 0.1,
-};
-
-let observer = new IntersectionObserver(slideAnim, options);
-
-function slideAnim(entries) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      slideTimeline.fromTo(
-        header,
-        { opacity: 0, x: "-20%" },
-        { opacity: 1, x: "0%" },
-        { onComplete: "done" }
-      );
-      slideTimeline.fromTo(
-        futureIllustration,
-        { opacity: 0, x: "-20%" },
-        { opacity: 1, x: "0%" },
-        "+=0.1"
-      );
-      slideTimeline.fromTo(
-        futureTextContent,
-        { opacity: 0, x: "-20%" },
-        { opacity: 1, x: "0%" },
-        "+=0.2"
-      );
-
-      elasticTimeline.fromTo(
-        mobileIllustration,
-        { opacity: 0, x: "-50%" },
-        { opacity: 1, x: "0%" },
-        "+=1.2"
-      );
-
-      slideTimeline.fromTo(
-        infrastructureText,
-        { opacity: 0, x: "20%" },
-        { opacity: 1, x: "0%" },
-        "-=1"
-      );
-
-      backTimeline.fromTo(
-        laptopIllustration,
-        { opacity: 0, scale: 0 },
-        { opacity: 1, scale: 1 },
-        "+=1.4"
-      );
-
-      backTimeline.fromTo(
-        toolsTextContent,
-        { opacity: 0, x: "50%" },
-        { opacity: 1, x: "0%" },
-        "-=1"
-      );
-
-      bounceTimeline.fromTo(
-        footerContent,
-        { opacity: 0, y: "10%" },
-        { opacity: 1, y: "0%" },
-        "+=1"
-      );
-    }
-  });
-}
-
-slides.forEach((slide) => {
-  observer.observe(slide);
 });
